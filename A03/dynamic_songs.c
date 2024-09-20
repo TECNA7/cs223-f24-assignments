2a17e1acb32877daff09ab73cffa43cc9e4c0d9d
Kripa Lamichhane <klamichhan@goldengate.cs.brynmawr.edu>
2024-09-20 19:49:52 -0400

    Assignment 3v completed

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,73 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
- ---------------------------------------------*/
+ * Author: Kripa Lamichhane 
+ * Date: 9/20/2024
+ * Description: File that that reads songs from a text file into an array
+ --------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char title[128];
+    char artist[128];
+    int duration[2]; 
+    float danceability;
+    float energy;
+    float tempo;
+    float valence;
+} Song;
+
+void prints(Song* songs, int size) {
+    printf("Welcome to Dynamic Donna's Danceability Directory.\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d) %-25s artist: %-20s duration: %d:%02d D: %.03f E: %.03f T: %.03f V: %.03f\n",
+            i, songs[i].title, songs[i].artist, songs[i].duration[0], songs[i].duration[1],
+            songs[i].danceability, songs[i].energy, songs[i].tempo, songs[i].valence);
+    }
+}
 
 int main() {
-  return 0;
+    FILE *songlist = fopen("songlist.csv", "r");
+    if (songlist == NULL) {
+        printf("Error: unable to open file.\n");
+        return 1;
+    }
+
+    char line[256];
+    fgets(line, sizeof(line), songlist);
+    int num_songs = atoi(strtok(line, ","));
+
+    Song *songs = malloc(sizeof(Song) * num_songs);
+    if (songs == NULL) {
+        printf("Error: unable to allocate memory.\n");
+        fclose(songlist);
+        return 1;
+    }
+
+    fgets(line, sizeof(line), songlist);
+
+    // Reading data from file
+    for (int i = 0; i < num_songs; i++) {
+        fgets(line, sizeof(line), songlist);
+
+        // Parsing the line
+        strcpy(songs[i].title, strtok(line, ","));
+        strcpy(songs[i].artist, strtok(NULL, ","));
+        int duration_ms = atoi(strtok(NULL, ",")); // Duration in milliseconds
+        songs[i].duration[0] = duration_ms / 60000;  //Duration in  Minutes
+        songs[i].duration[1] = (duration_ms % 60000) / 1000; // Durationm Seconds
+        songs[i].danceability = atof(strtok(NULL, ","));
+        songs[i].energy = atof(strtok(NULL, ","));
+        songs[i].tempo = atof(strtok(NULL, ","));
+        songs[i].valence = atof(strtok(NULL, ","));
+    }
+
+    print_songs(songs, num_songs);
+
+    // Freed
+    free(songs);
+    fclose(songlist);
+
+    return 0;
 }
+
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,49 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Kripa Lamichhane
+ * Date: 9/20/2024
+ * Description: This is a program that turns a phrase into ent-speech. 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  return 0;
+    int Pause, Length, i, j;
+    char input[32], *slow;
+
+    printf("Pause length: ");
+    if (scanf("%d", &Pause) != 1) {
+        printf("Invalid length\n");
+        return 1;
+    }
+
+    printf("Text: ");
+    if (scanf("%31s", input) != 1) {
+        printf("Invaid text\n");
+        return 1;
+    }
+    Length = strlen(input);
+
+    slow = (char *)malloc((Length * 2 + 1) * sizeof(char)); 
+    if (!slow) {
+        printf("There's memory leak\n");
+        return 2;
+    }
+
+    for (i = 0, j = 0; i < Length; i++) {
+        slow[j++] = input[i];
+        if (i < Length - 1) slow[j++] = '.';
+    }
+    slow[j] = '\0'; 
+
+    for (i = 0; slow[i] != '\0'; i++) {
+        printf("%c", slow[i]);
+        if (slow[i] == '.') {
+            for (j = 0; j < Pause; j++) printf(".");
+        }
+    }
+    printf("\n");
+
+    free(slow);
+    return 0;
 }
