06d909f4421998b164048598e23889197e6c58c0
Kripa Lamichhane <klamichhan@goldengate.cs.brynmawr.edu>
2024-11-02 00:30:59 -0400

    Assignment 8 done again again

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,3 +1,7 @@
+/**Author : KRIPA LAMICHHANE
+ * DATE : 11/1/2024
+ * DESCRI[TIPON : WARM UP TO PRINT CHILD AND PARENTS USING FORK
+ * **/
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -6,29 +10,28 @@
 int main() {
     pid_t pid;
 
-    // A0 process (main process)
     printf("[%d] A0\n", getpid());
     fflush(stdout);
 
     pid = fork();
-    if (pid) {  // Parent process (A0, acting as B0 after first fork)
+    if (pid) { 
         printf("[%d] B0\n", getpid());
         fflush(stdout);
 
         pid = fork();
-        if (pid) {  // Still in the parent process (A0, acting as B1 after second fork)
+        if (pid) {  
             printf("[%d] B1\n", getpid());
             fflush(stdout);
-            printf("[%d] Bye\n", getpid());  // B1 says Bye
-        } else {  // Child process created by second fork (C1)
+            printf("[%d] Bye\n", getpid());  
+        } else { 
             printf("[%d] C1\n", getpid());
             fflush(stdout);
-            printf("[%d] Bye\n", getpid());  // C1 says Bye
+            printf("[%d] Bye\n", getpid());  
         }
-    } else {  // Child process created by the first fork (C0)
+    } else {
         printf("[%d] C0\n", getpid());
         fflush(stdout);
-        printf("[%d] Bye\n", getpid());  // C0 says Bye
+        printf("[%d] Bye\n", getpid()); 
     }
 
     return 0;
